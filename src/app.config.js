7f411d592bd0eb10d310084753bb1f2320a874a8
xujian <908634674@qq.com>
2023-04-27 09:23:48 +0800

    输入框案例

--- src/app.config.js
+++ src/app.config.js
@@ -10,11 +10,4 @@ const config = defineAppConfig({
   }
 })
 
-const arr = [1, 2, 3, 4, 5]
-
-console.log(arr.map(v => {
-  console.log('箭头函数不会被执行，且除了剪头函数之外，还有多个es6的语法似乎也不执行，3.6.2版本上是正常的')
-  return v + 2
-}))
-
 export default config
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,24 +1,28 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import { useState } from 'react'
+import { View, Input, Button } from '@tarojs/components'
 import './index.scss'
 
-export default class Index extends Component {
+export default function Index() {
 
-  componentWillMount () { }
+  const [values, setValues] = useState({})
 
-  componentDidMount () { }
+  const [show, setShow] = useState(false)
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
-    )
-  }
+  return <View className='index'>
+    <Input
+      value={values.value1}
+      onInput={e => setValues({ ...values, value1: e.detail.value })}
+      placeholder='输入1'
+    />
+    <Button onClick={() => setShow(true)}>按钮</Button>
+    <View>输入1为空点击按钮会直接报错，输入1不为空，点击按钮，再输入输入1也会报错</View>
+    <View>当绑定的值为空的时候就会报错</View>
+    {
+      show && <Input
+        value={values.test}
+        onInput={e => setValues({ ...values, test: e.detail.value })}
+        placeholder='输入2'
+      />
+    }
+  </View>
 }
